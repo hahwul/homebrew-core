0cb1e51785d5130d28ab567b005dc61dbaa4c817
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-03-07 22:29:45 +0800

    fypp: drop `python-setuptools` dependency

--- Formula/f/fypp.rb
+++ Formula/f/fypp.rb
@@ -1,9 +1,12 @@
 class Fypp < Formula
+  include Language::Python::Virtualenv
+
   desc "Python powered Fortran preprocessor"
   homepage "https://fypp.readthedocs.io/en/stable/"
   url "https://files.pythonhosted.org/packages/01/35/0e2dfffc90201f17436d3416f8d5c8b00e2187e410ec899bb62cf2cea59b/fypp-3.2.tar.gz"
   sha256 "05c20f71dd9a7206ffe2d8688032723f97b8c2984d472ba045819d7d2b513bce"
   license "BSD-2-Clause"
+  revision 1
   head "https://github.com/aradi/fypp.git", branch: "main"
 
   bottle do
@@ -17,16 +20,16 @@ class Fypp < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "7c2b773c9ffd79f4f5adec76d7a01eae787f7e175de659e44e865628042690f9"
   end
 
-  depends_on "python-setuptools" => :build
   depends_on "gcc" => :test
   depends_on "python@3.12"
 
-  def python3
-    "python3.12"
+  resource "setuptools" do
+    url "https://files.pythonhosted.org/packages/c8/1f/e026746e5885a83e1af99002ae63650b7c577af5c424d4c27edcf729ab44/setuptools-69.1.1.tar.gz"
+    sha256 "5c0806c7d9af348e6dd3777b4f4dbb42c7ad85b190104837488eab9a7c945cf8"
   end
 
   def install
-    system python3, "-m", "pip", "install", *std_pip_args, "."
+    virtualenv_install_with_resources
   end
 
   test do
