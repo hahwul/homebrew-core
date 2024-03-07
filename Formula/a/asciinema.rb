2b46561f9632bc9f0b0b671aebb32088f74f1d1b
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-03-07 22:06:53 +0800

    asciinema: drop `python-setuptools` dependency

--- Formula/a/asciinema.rb
+++ Formula/a/asciinema.rb
@@ -1,9 +1,12 @@
 class Asciinema < Formula
+  include Language::Python::Virtualenv
+
   desc "Record and share terminal sessions"
   homepage "https://asciinema.org"
   url "https://files.pythonhosted.org/packages/f1/19/45b405438e90ad5b9618f3df62e9b3edaa2b115b530e60bd4b363465c704/asciinema-2.4.0.tar.gz"
   sha256 "828e04c36ba622a7b8f8f912c8f0c1329538b6c7ed1c0d1b131bbbfe3a221707"
   license "GPL-3.0-only"
+  revision 1
   head "https://github.com/asciinema/asciinema.git", branch: "develop"
 
   bottle do
@@ -17,15 +20,15 @@ class Asciinema < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "ecb93ab4fa6cec9f6346eefd2d9ddac8dc5a981a8c946494fa12b5784b5ad669"
   end
 
-  depends_on "python-setuptools" => :build
   depends_on "python@3.12"
 
-  def python3
-    which("python3.12")
+  resource "setuptools" do
+    url "https://files.pythonhosted.org/packages/c8/1f/e026746e5885a83e1af99002ae63650b7c577af5c424d4c27edcf729ab44/setuptools-69.1.1.tar.gz"
+    sha256 "5c0806c7d9af348e6dd3777b4f4dbb42c7ad85b190104837488eab9a7c945cf8"
   end
 
   def install
-    system python3, "-m", "pip", "install", *std_pip_args, "."
+    virtualenv_install_with_resources
   end
 
   test do
